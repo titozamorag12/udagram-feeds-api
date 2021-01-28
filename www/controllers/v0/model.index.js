7abf4da72f3e0641b1b7e184ad433b7b58b84028
gzamora <gzamora@cartfulsolutions.com>
2021-01-27 18:09:08 -0600

    Microservice for feeds service API

---
+++ www/controllers/v0/model.index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const FeedItem_1 = require("./feed/models/FeedItem");
+exports.V0_FEED_MODELS = [FeedItem_1.FeedItem];
+//# sourceMappingURL=model.index.js.map
